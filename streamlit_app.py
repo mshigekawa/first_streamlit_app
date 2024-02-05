49abafd9c9b5c6fcbc92007adca2aab3aaa61fa9
Matt <mshigekawa@gmail.com>
2024-02-05 13:14:09 -0800

    Update streamlit_app.py
    added back snowflake call

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+# call snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)
 
